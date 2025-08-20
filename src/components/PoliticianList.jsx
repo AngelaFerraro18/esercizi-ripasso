d78b2f519945c797a9b8f8d84d9426694cfe5aad
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-08-20 17:23:43 +0200

    feat: milestone 2 completed

--- src/components/PoliticianList.jsx
+++ src/components/PoliticianList.jsx
@@ -1,8 +1,9 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 
 function PoliticianList() {
 
     const [list, setList] = useState([]);
+    const [search, setSearch] = useState('');
 
     async function politicsFetch() {
         const response = await fetch('http://localhost:3333/politicians');
@@ -12,19 +13,24 @@ function PoliticianList() {
 
     };
 
-    useEffect(() => politicsFetch, [])
+    const filteredPoliticians = useMemo(() => {
+        const filtered = list.filter(p => p.name.toLowerCase().includes(search.toLowerCase()));
+        return filtered;
+    }, [search, list])
+
+    useEffect(() => { politicsFetch() }, []);
 
     return (
         <>
             <h1>Lista politici</h1>
-
+            <input type="text" value={search} onChange={e => setSearch(e.target.value)} />
             <ul>
-                {list && list.map(p => <li key={p.id}>
+                {filteredPoliticians.length > 0 ? (filteredPoliticians.map(p => <li key={p.id}>
                     <h4>{p.name}</h4>
                     <img src={p.image} alt={p.name} />
                     <p>Position: {p.position}</p>
                     <p>Bio: {p.biography}</p>
-                </li>)}
+                </li>)) : <p>Nessun politico trovato!</p>}
             </ul>
         </>
     )
