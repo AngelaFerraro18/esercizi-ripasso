239bee8fe630859f7614404f581fcf8fc7f89292
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-08-22 11:23:30 +0200

    feat: milestone 2 completed

--- src/components/Spesa.jsx
+++ src/components/Spesa.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+
+
 const products = [
     { name: 'Mela', price: 0.5 },
     { name: 'Pane', price: 1.2 },
@@ -8,15 +11,36 @@ const products = [
 
 function Spesa() {
 
+    const [addedProduct, setAddedProduct] = useState([]);
+
+    function addToCart(product) {
+        setAddedProduct(prev => {
+            if (prev.some(item => item.name === product.name)) return prev;
+            return [...prev, { ...product, quantity: 1 }]
+        })
+    }
     return (
         <>
             <h2>Lista della spesa</h2>
             <ul>
                 {products.map((p, index) => <li key={index}>
                     <h4>{p.name}</h4>
-                    <span> Prezzo: {p.prezzo}</span>
+                    <span> Prezzo: {p.price}</span>
+                    <button onClick={() => addToCart(p)}>Aggiungi al carrello</button>
                 </li>)}
             </ul>
+            {
+                addedProduct.length > 0 && (
+                    <>
+                        <h3>Carrello</h3>
+                        <ul>
+                            {addedProduct.map((p, index) => <li key={index}>
+                                {p.name} - {p.price} --- Quantità: {p.quantity}
+                            </li>)}
+                        </ul>
+                    </>
+                )
+            }
         </>
     )
 }
